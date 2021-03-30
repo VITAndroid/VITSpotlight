4137c84f07f6eb6057ac6d88f2fd46e67fb5e4ba
sangeeth-kumar <sangeethkumar.t2017@vitstudent.ac.in>
2021-03-30 21:08:15 +0530

    First Commit - User Access Management

---
+++ app/src/main/java/com/example/vit_spotlight/Login.java
@@ -0,0 +1,196 @@
+package com.example.vit_spotlight;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Login extends AppCompatActivity {
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    TextView forgotTextLink;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
+
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        gotoRegister = findViewById(R.id.gotoRegister);
+        forgotTextLink = findViewById(R.id.forgotPassword);
+
+        loginBtn.setOnClickListener(v -> {
+            checkField(email);
+            checkField(password);
+
+            if(valid) {
+                fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (!user.isEmailVerified()){
+
+                            AlertDialog.Builder emailAlert  = new AlertDialog.Builder(Login.this);
+                            emailAlert.setTitle("Verify you email");
+                            emailAlert.setMessage("The email address is not verified.");
+                            emailAlert.setCancelable(false);
+                            emailAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    fAuth.signOut();
+                                    return;
+                                }
+                            });
+                            emailAlert.setNegativeButton("Send Verify", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    user.sendEmailVerification()
+                                            .addOnCompleteListener(task -> {
+                                                if (task.isSuccessful()){
+                                                    Toast.makeText(Login.this, "Verify Link Sent to Email", Toast.LENGTH_SHORT).show();
+                                                }
+                                                fAuth.signOut();
+                                                return;
+                                            });
+                                }
+                            });
+                            emailAlert.show();
+                        }
+                        if(user.isEmailVerified()){
+                        Toast.makeText(Login.this, "Logged in Successful", Toast.LENGTH_SHORT).show();
+                        checkUserAccessLevel(authResult.getUser().getUid());
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, "Failed to Login", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        gotoRegister.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Register.class)));
+
+        forgotTextLink.setOnClickListener(v -> {
+
+            final EditText resetMail = new EditText(v.getContext());
+            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+            passwordResetDialog.setTitle("Reset Password ?");
+            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+            passwordResetDialog.setView(resetMail);
+            passwordResetDialog.setCancelable(false);
+
+            passwordResetDialog.setPositiveButton("Yes", (dialog, which) -> {
+                // extract the email and send reset link
+                String mail = resetMail.getText().toString();
+                fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Login.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            });
+
+            passwordResetDialog.setNegativeButton("No", (dialog, which) -> {
+                // close the dialog
+            });
+
+            passwordResetDialog.create().show();
+
+        });
+
+    }
+
+    private void checkUserAccessLevel(String uid) {
+        DocumentReference df=fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG", "On Success"+documentSnapshot.getData());
+                if(documentSnapshot.getString("isClub")!=null){
+                    startActivity(new Intent(getApplicationContext(),Club.class));
+                    finish();
+                }
+                if(documentSnapshot.getString("isStudent")!=null){
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
+            }
+        });
+
+
+
+    }
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            DocumentReference df= FirebaseFirestore.getInstance().collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot.getString("isClub")!=null){
+                        startActivity(new Intent(getApplicationContext(),Club.class));
+                        finish();
+                    }
+                    if(documentSnapshot.getString("isStudent")!=null){
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(getApplicationContext(),Login.class));
+                    finish();
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/vit_spotlight/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.vit_spotlight;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth fAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button logout=findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+            }
+        });
+    }
+}

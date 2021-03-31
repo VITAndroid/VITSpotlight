7a4796bbb5cb513f234509a280bd8f3c0e011c76
sangeeth-kumar <sangeethkumar.t2017@vitstudent.ac.in>
2021-03-31 11:25:34 +0530

    First Commit - User Access Management

---
+++ app/src/main/java/com/example/vit_spotlight/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.vit_spotlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+            }
+        }, 4000);
+
+
+    }
+}
